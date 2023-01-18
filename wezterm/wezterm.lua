6fac7a676492e1135cb6612a91a3f0ab1112a75c
Dawid <david@ricsys.co.za>
2023-01-18 21:41:21 +0200

    Worked oin wezterm..... FUCK LUA... for now

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,5 +1,19 @@
+local wezterm = require "wezterm"
 return {
   color_scheme = 'tokyonight',
-  window_background_opacity = 0.9,
+  window_background_opacity = 0.87,
 
-} 
+  -- font = wezterm.font 'Fira Code',
+  hide_tab_bar_if_only_one_tab = true,
+  tab_bar_at_bottom = true,
+
+  warn_about_missing_glyphs = false,
+
+
+  -- Set up new keybinds... Going to set it up as a browser..
+  keys = {
+    { key = "Tab", mods = 'CTRL', action = wezterm.action.SpawnWindow },
+    { key = "b", mods = 'CTRL', action = wezterm.action.ActivateTabRelative },
+  },
+
+}
