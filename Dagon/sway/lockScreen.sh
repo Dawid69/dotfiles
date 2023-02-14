2ed70c3236bd5953808ed18702f44bff9061677a
David <jordaandvd@gmail.com>
2023-02-14 13:59:35 +0200

    Fixed Lockscreen stuff

--- Dagon/sway/lockScreen.sh
+++ Dagon/sway/lockScreen.sh
@@ -3,8 +3,9 @@
 swaylock \
 	--screenshots \
 	--clock \
+	--datestr '%a %d/%m/%y' \
 	--indicator \
-	--indicator-radius 100 \
+	--indicator-radius 120 \
 	--indicator-thickness 7 \
 	--effect-blur 7x5 \
 	--effect-vignette 0.5:0.5 \
@@ -13,5 +14,5 @@ swaylock \
 	--line-color 00000000 \
 	--inside-color 00000088 \
 	--separator-color 00000000 \
-	--grace 2 \
+	--grace 5 \
 	--fade-in 0.2
