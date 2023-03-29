96b4346d1d76af386dca3f7bb03e8a55366f9c85
David <jordaandvd@gmail.com>
2023-03-29 09:42:17 +0200

    formatting I think

--- nvim_user_config/init.lua
+++ nvim_user_config/init.lua
@@ -1,279 +1,267 @@
 local config = {
-
-  -- Set colorscheme
-  colorscheme = "tokyonight-night",
-
-  -- set vim options here (vim.<first_key>.<second_key> =  value)
-  options = {
-    opt = {
-      relativenumber = false, -- sets vim.opt.relativenumber
-    },
-    g = {
-      mapleader = " ", -- sets vim.g.mapleader
-      tokyonight_style = "night",
-    },
-  },
-
-
-  -- Disable AstroNvim ui features
-  ui = {
-    nui_input = true,
-    telescope_select = true,
-  },
-
-  -- Configure plugins
-  plugins = {
-
-    -- Debug adapter for rust
-    ["mason-nvim-dap"] = {
-      ensure_installed = { "codelldb" },
-    },
-
-    init = {
-      { "wfxr/minimap.vim" },
-      { "folke/tokyonight.nvim" },
-      {
-        "epwalsh/obsidian.nvim",
-        config = function()
-          require("obsidian").setup {
-            dir = "~/Documents/Obsidian/RicSys",
-            completion = {
-              nvim_cmp = true,
-            }
-          }
-        end
-      },
-      {
-        --- Zenmode for editing
-        "folke/zen-mode.nvim",
-        config = function()
-          require("zen-mode").setup {
-            window = {
-              backdrop = 0.9,
-              width = 0.85,
-            },
-          }
-
-        end
-      },
-
-      -- Rust support
-      {
-        "simrat39/rust-tools.nvim",
-        after = { "mason-lspconfig.nvim" },
-        -- Is configured via the server_registration_override installed below!
-        config = function()
-          require("rust-tools").setup {
-            server = astronvim.lsp.server_settings "rust_analyzer",
-            tools = {
-              inlay_hints = {
-                right_aling = true,
-                right_align_padding = 7,
-              },
-            },
-          }
-        end,
-      },
-
-      {
-        "Saecki/crates.nvim",
-        event = { "BufRead Cargo.toml" },
-        config = function()
-          require("crates").setup()
-          astronvim.add_cmp_source { name = "crates", priority = 1100 }
-
-          -- Crates mappings:
-          local map = vim.api.nvim_set_keymap
-          map("n", "<leader>Ct", ":lua require('crates').toggle()<cr>", { desc = "Toggle extra crates.io information" })
-          map("n", "<leader>Cr", ":lua require('crates').reload()<cr>", { desc = "Reload information from crates.io" })
-          map("n", "<leader>CU", ":lua require('crates').upgrade_crate()<cr>", { desc = "Upgrade a crate" })
-          map("v", "<leader>CU", ":lua require('crates').upgrade_crates()<cr>", { desc = "Upgrade selected crates" })
-          map("n", "<leader>CA", ":lua require('crates').upgrade_all_crates()<cr>", { desc = "Upgrade all crates" })
-        end,
-      },
-    },
-
-    -- All other entries override the setup() call for default plugins
-    ["null-ls"] = function(config)
-      local null_ls = require "null-ls"
-      -- Check supported formatters and linters
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
-      -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
-      config.sources = {
-        -- Set a formatter
-        null_ls.builtins.formatting.rufo,
-        -- Set a linter
-        null_ls.builtins.diagnostics.rubocop,
-      }
-      -- set up null-ls's on_attach function
-      config.on_attach = function(client)
-        -- NOTE: You can remove this on attach function to disable format on save
-        if client.resolved_capabilities.document_formatting then
-          vim.api.nvim_create_autocmd("BufWritePre", {
-            desc = "Auto format before save",
-            pattern = "<buffer>",
-            callback = vim.lsp.buf.formatting_sync,
-          })
-        end
-      end
-      return config -- return final config table
-    end,
-
-
-    treesitter = {
-      ensure_installed = { "lua" },
-    },
-    ["nvim-lsp-installer"] = {
-      ensure_installed = { "rust_analyzer" },
-    },
-    packer = {
-      compile_path = vim.fn.stdpath "config" .. "/lua/packer_compiled.lua",
-    },
-  },
-
-  -- LuaSnip Options
-  luasnip = {
-    -- Add paths for including more VS Code style snippets in luasnip
-    vscode_snippet_paths = {},
-    -- Extend filetypes
-    filetype_extend = {
-      javascript = { "javascriptreact" },
-    },
-  },
-
-  -- Modify which-key registration
-  ["which-key"] = {
-    -- Add bindings
-    register_mappings = {
-      -- first key is the mode, n == normal mode
-      n = {
-        -- second key is the prefix, <leader> prefixes
-        ["<leader>"] = {
-          -- ["C"] = { "", "Crates" },
-          -- which-key registration table for normal mode, leader prefix
-          -- ["N"] = { "<cmd>tabnew<cr>", "New Buffer" },
+    -- Set colorscheme
+    colorscheme = "tokyonight-night",
+    -- set vim options here (vim.<first_key>.<second_key> =  value)
+    options = {
+        opt = {
+            relativenumber = false, -- sets vim.opt.relativenumber
         },
-      },
+        g = {
+            mapleader = " ", -- sets vim.g.mapleader
+            tokyonight_style = "night",
+        },
     },
-  },
-
-  -- CMP Source Priorities
-  -- modify here the priorities of default cmp sources
-  -- higher value == higher priority
-  -- The value can also be set to a boolean for disabling default sources:
-  -- false == disabled
-  -- true == 1000
-  cmp = {
-    source_priority = {
-      nvim_lsp = 1000,
-      luasnip = 750,
-      buffer = 500,
-      path = 250,
+    -- Disable AstroNvim ui features
+    ui = {
+        nui_input = true,
+        telescope_select = true,
     },
-  },
+    -- Configure plugins
+    plugins = {
 
-  -- Extend LSP configuration
-  lsp = {
-    -- enable servers that you already have installed without lsp-installer
-    servers = {
-      -- "pyright"
+        -- Debug adapter for rust
+        ["mason-nvim-dap"] = {
+            ensure_installed = { "codelldb" },
+        },
+
+        init = {
+            { "folke/tokyonight.nvim" },
+            {
+                "epwalsh/obsidian.nvim",
+                config = function()
+                  require("obsidian").setup {
+                      dir = "~/Documents/Obsidian/RicSys",
+                      completion = {
+                          nvim_cmp = true,
+                      }
+                  }
+                end
+            },
+            {
+                --- Zenmode for editing
+                "folke/zen-mode.nvim",
+                config = function()
+                  require("zen-mode").setup {
+                      window = {
+                          backdrop = 0.9,
+                          width = 0.85,
+                      },
+                  }
+                end
+            },
+
+            -- Rust support
+            {
+                "simrat39/rust-tools.nvim",
+                after = { "mason-lspconfig.nvim" },
+                -- Is configured via the server_registration_override installed below!
+                config = function()
+                  require("rust-tools").setup {
+                      server = astronvim.lsp.server_settings "rust_analyzer",
+                      tools = {
+                          inlay_hints = {
+                              right_aling = true,
+                              right_align_padding = 7,
+                          },
+                      },
+                  }
+                end,
+            },
+
+            {
+                "Saecki/crates.nvim",
+                event = { "BufRead Cargo.toml" },
+                config = function()
+                  require("crates").setup()
+                  astronvim.add_cmp_source { name = "crates", priority = 1100 }
+
+                  -- Crates mappings:
+                  local map = vim.api.nvim_set_keymap
+                  map("n", "<leader>Ct", ":lua require('crates').toggle()<cr>",
+                      { desc = "Toggle extra crates.io information" })
+                  map("n", "<leader>Cr", ":lua require('crates').reload()<cr>",
+                      { desc = "Reload information from crates.io" })
+                  map("n", "<leader>CU", ":lua require('crates').upgrade_crate()<cr>", { desc = "Upgrade a crate" })
+                  map("v", "<leader>CU", ":lua require('crates').upgrade_crates()<cr>",
+                      { desc = "Upgrade selected crates" })
+                  map("n", "<leader>CA", ":lua require('crates').upgrade_all_crates()<cr>",
+                      { desc = "Upgrade all crates" })
+                end,
+            },
+        },
+
+        -- All other entries override the setup() call for default plugins
+        ["null-ls"] = function(config)
+          local null_ls = require "null-ls"
+          -- Check supported formatters and linters
+          -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/formatting
+          -- https://github.com/jose-elias-alvarez/null-ls.nvim/tree/main/lua/null-ls/builtins/diagnostics
+          config.sources = {
+              -- Set a formatter
+              null_ls.builtins.formatting.rufo,
+              -- Set a linter
+              null_ls.builtins.diagnostics.rubocop,
+          }
+          -- set up null-ls's on_attach function
+          config.on_attach = function(client)
+            -- NOTE: You can remove this on attach function to disable format on save
+            if client.resolved_capabilities.document_formatting then
+              vim.api.nvim_create_autocmd("BufWritePre", {
+                  desc = "Auto format before save",
+                  pattern = "<buffer>",
+                  callback = vim.lsp.buf.formatting_sync,
+              })
+            end
+          end
+          return config -- return final config table
+        end,
+
+
+        treesitter = {
+            ensure_installed = { "lua" },
+        },
+        ["nvim-lsp-installer"] = {
+            ensure_installed = { "rust_analyzer" },
+        },
+        packer = {
+            compile_path = vim.fn.stdpath "config" .. "/lua/packer_compiled.lua",
+        },
     },
-    -- add to the server on_attach function
-    -- on_attach = function(client, bufnr)
-    -- end,
+    -- LuaSnip Options
+    luasnip = {
+        -- Add paths for including more VS Code style snippets in luasnip
+        vscode_snippet_paths = {},
+        -- Extend filetypes
+        filetype_extend = {
+            javascript = { "javascriptreact" },
+        },
+    },
+    -- Modify which-key registration
+    ["which-key"] = {
+        -- Add bindings
+        register_mappings = {
+            -- first key is the mode, n == normal mode
+            n = {
+                -- second key is the prefix, <leader> prefixes
+                ["<leader>"] = {
+                    -- ["C"] = { "", "Crates" },
+                    -- which-key registration table for normal mode, leader prefix
+                    -- ["N"] = { "<cmd>tabnew<cr>", "New Buffer" },
+                },
+            },
+        },
+    },
+    -- CMP Source Priorities
+    -- modify here the priorities of default cmp sources
+    -- higher value == higher priority
+    -- The value can also be set to a boolean for disabling default sources:
+    -- false == disabled
+    -- true == 1000
+    cmp = {
+        source_priority = {
+            nvim_lsp = 1000,
+            luasnip = 750,
+            buffer = 500,
+            path = 250,
+        },
+    },
+    -- Extend LSP configuration
+    lsp = {
+        -- enable servers that you already have installed without lsp-installer
+        servers = {
+            -- "pyright"
+        },
+        -- add to the server on_attach function
+        -- on_attach = function(client, bufnr)
+        -- end,
 
-    -- override the lsp installer server-registration function
-    -- server_registration = function(server, opts)
-    --   require("lspconfig")[server].setup(opts)
-    -- end,
+        -- override the lsp installer server-registration function
+        -- server_registration = function(server, opts)
+        --   require("lspconfig")[server].setup(opts)
+        -- end,
 
-    -- Add overrides for LSP server settings, the keys are the name of the server
-    ["server-settings"] = {
-      arduino_language_server = {
-        cmd = {
-          "/home/dawid/.local/share/nvim/lsp_servers/arduino_language_server/arduino-language-server",
-          -- "-clangd", "/usr/bin/clangd",
-          -- "-cli", "/usr/bin/arduino-cli",
-          "-cli-config", "/home/dawid/.arduino15/arduino-cli.yaml",
-          -- "-fqbn", "ATTinyCore:avr:attinyx4opti"
-          "-fqbn", "arduino:avr:uno"
-        }
-      },
-      -- clangd = {
-      --   cmd = {
-      --     "clangd",
-      --     "--query-driver=/bin/arm-none-eabi-*",
-      --   }
-      -- }
+        -- Add overrides for LSP server settings, the keys are the name of the server
+        ["server-settings"] = {
+            arduino_language_server = {
+                cmd = {
+                    "/home/dawid/.local/share/nvim/lsp_servers/arduino_language_server/arduino-language-server",
+                    -- "-clangd", "/usr/bin/clangd",
+                    -- "-cli", "/usr/bin/arduino-cli",
+                    "-cli-config", "/home/dawid/.arduino15/arduino-cli.yaml",
+                    -- "-fqbn", "ATTinyCore:avr:attinyx4opti"
+                    "-fqbn", "arduino:avr:uno"
+                }
+            },
+            -- clangd = {
+            --   cmd = {
+            --     "clangd",
+            --     "--query-driver=/bin/arm-none-eabi-*",
+            --   }
+            -- }
 
-      -- example for addings schemas to yamlls
-      -- yamlls = {
-      --   settings = {
-      --     yaml = {
-      --       schemas = {
-      --         ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
-      --         ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
-      --         ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
-      --       },
-      --     },
+            -- example for addings schemas to yamlls
+            -- yamlls = {
+            --   settings = {
+            --     yaml = {
+            --       schemas = {
+            --         ["http://json.schemastore.org/github-workflow"] = ".github/workflows/*.{yml,yaml}",
+            --         ["http://json.schemastore.org/github-action"] = ".github/action.{yml,yaml}",
+            --         ["http://json.schemastore.org/ansible-stable-2.9"] = "roles/tasks/*.{yml,yaml}",
+            --       },
+            --     },
+            --   },
+            -- },
+        },
+    },
+    -- Diagnostics configuration (for vim.diagnostics.config({}))
+    diagnostics = {
+        virtual_text = true,
+        underline = true,
+    },
+    header = {
+
+
+        " ███    ██ ██████  ██████  ██    ██ ██ ███    ███ ",
+        " ████   ██ ██     ██    ██ ██    ██ ██ ████  ████ ",
+        " ██ ██  ██ ████   ██    ██ ██    ██ ██ ██ ████ ██ ",
+        " ██  ██ ██ ██     ██    ██  ██  ██  ██ ██  ██  ██ ",
+        " ██   ████ ██████  ██████    ████   ██ ██      ██ ",
+    },
+    -- This function is run last
+    -- good place to configure mappings and vim options
+    polish = function()
+      local map = vim.api.nvim_set_keymap
+      local unmap = vim.api.nvim_del_keymap
+
+
+      -- Set key bindings
+      vim.keymap.set("n", "<C-s>", ":w!<CR>")
+
+      unmap("n", "<leader>C")
+
+
+      -- Set autocommands
+      vim.api.nvim_create_augroup("packer_conf", { clear = true })
+      vim.api.nvim_create_autocmd("BufWritePost", {
+          desc = "Sync packer after modifying plugins.lua",
+          group = "packer_conf",
+          pattern = "plugins.lua",
+          command = "source <afile> | PackerSync",
+      })
+
+      -- Set up custom filetypes
+      -- vim.filetype.add {
+      --   extension = {
+      --     foo = "fooscript",
       --   },
-      -- },
-    },
-  },
-
-  -- Diagnostics configuration (for vim.diagnostics.config({}))
-  diagnostics = {
-    virtual_text = true,
-    underline = true,
-  },
-  header = {
-
-
-    " ███    ██ ██████  ██████  ██    ██ ██ ███    ███ ",
-    " ████   ██ ██     ██    ██ ██    ██ ██ ████  ████ ",
-    " ██ ██  ██ ████   ██    ██ ██    ██ ██ ██ ████ ██ ",
-    " ██  ██ ██ ██     ██    ██  ██  ██  ██ ██  ██  ██ ",
-    " ██   ████ ██████  ██████    ████   ██ ██      ██ ",
-  },
-
-  -- This function is run last
-  -- good place to configure mappings and vim options
-  polish = function()
-
-
-
-    local map = vim.api.nvim_set_keymap
-    local unmap = vim.api.nvim_del_keymap
-
-
-    -- Set key bindings
-    vim.keymap.set("n", "<C-s>", ":w!<CR>")
-
-    unmap("n", "<leader>C")
-
-
-    -- Set autocommands
-    vim.api.nvim_create_augroup("packer_conf", { clear = true })
-    vim.api.nvim_create_autocmd("BufWritePost", {
-      desc = "Sync packer after modifying plugins.lua",
-      group = "packer_conf",
-      pattern = "plugins.lua",
-      command = "source <afile> | PackerSync",
-    })
-
-    -- Set up custom filetypes
-    -- vim.filetype.add {
-    --   extension = {
-    --     foo = "fooscript",
-    --   },
-    --   filename = {
-    --     ["Foofile"] = "fooscript",
-    --   },
-    --   pattern = {
-    --     ["~/%.config/foo/.*"] = "fooscript",
-    --   },
-    -- }
-  end,
+      --   filename = {
+      --     ["Foofile"] = "fooscript",
+      --   },
+      --   pattern = {
+      --     ["~/%.config/foo/.*"] = "fooscript",
+      --   },
+      -- }
+    end,
 }
 
 return config
