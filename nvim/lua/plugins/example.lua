82b5765ebd940838f2b15cdc85fa66b72d76d144
Dawid <david@ricsys.co.za>
2023-07-23 21:48:12 +0200

    nvim shit

--- nvim/lua/plugins/example.lua
+++ nvim/lua/plugins/example.lua
@@ -142,7 +142,7 @@ return {
     opts = {
       ensure_installed = {
         "bash",
-        "json",
+        --"json",
         "lua",
         "markdown",
         "markdown_inline",
@@ -191,8 +191,9 @@ return {
   -- use mini.starter instead of alpha
   { import = "lazyvim.plugins.extras.ui.mini-starter" },
 
+  -- jsonls not needed?
   -- add jsonls and schemastore packages, and setup treesitter for json, json5 and jsonc
-  { import = "lazyvim.plugins.extras.lang.json" },
+  --{ import = "lazyvim.plugins.extras.lang.json" },
 
   -- add any tools you want to have installed below
   {
