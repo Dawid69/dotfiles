ca82d26ed1a3d99b7127bca6d7b167ef87da1eb0
Dawid <david@ricsys.co.za>
2024-10-18 09:53:45 +0200

    new commit

--- Azathoth/ironbar/scripts/numlock_azathoth.sh
+++ Azathoth/ironbar/scripts/numlock_azathoth.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 while read line; do
-	if [ "$line" = "1" ]; then
-		echo " 󰤇  "
-	else
-		echo "."
-	fi
+  if [ "$line" = "1" ]; then
+    echo " 󰤇  "
+  else
+    echo "."
+  fi
 done </sys/class/leds/input2::numlock/brightness
--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -5,7 +5,6 @@
 local keymap = vim.api.nvim_set_keymap
 local default_opts = { noremap = true, silent = true }
 local expr_opts = { noremap = true, expr = true, silent = true }
-
 --keymap("n", "<leader>x", ":buf", default_opts)
 
 -- jj to escape input mode...
@@ -25,11 +24,9 @@ keymap("v", "p", '"_dP', default_opts)
 
 -- Cancel search highlighting with ESC
 keymap("n", "<ESC>", ":nohlsearch<Bar>:echo<CR>", default_opts)
-
 -- Switch buffer
 keymap("n", "<S-h>", ":bprevious<CR>", default_opts)
 keymap("n", "<S-l>", ":bnext<CR>", default_opts)
-
 -- Move selected line / block of text in visual mode
 keymap("x", "K", ":move '<-2<CR>gv-gv", default_opts)
 keymap("x", "J", ":move '>+1<CR>gv-gv", default_opts)
